4011739a03c258055f67cbf7138941785e4e8686
Will Merydith <will.merydith@gmail.com>
2011-05-21 11:39:45 -0500

    Tests for home page.

--- admin.py
+++ admin.py
@@ -211,6 +211,41 @@ class Test(webapp.RequestHandler):
                 'battles': simplejson.dumps(battles_json)
             }        
             generate(self, 'test/test_map.html', template_values)
+
+class Home(webapp.RequestHandler):  
+    """Generates Morningstar home page.
+    """
+    def get(self):
+        _trace = TRACE+'Home:: get() '
+        logging.info(_trace)        
+        battles = models.BattlePin.all().fetch(100)
+        monsters = models.MonsterPartyPin.all().fetch(100)
+        players = models.PlayerPartyPin.all().fetch(100) 
+        battles_json = []
+        monsters_json = []
+        players_json = []
+        for b in battles:
+            lat, lon = utils.parseGeoPt(b.location)
+            data = {'name': b.name,'lat': lat,'lon': lon}
+            battles_json.append(data)
+        for m in monsters:
+            lat, lon = utils.parseGeoPt(m.location)
+            data = {'name': m.name,'lat': lat,'lon': lon}
+            monsters_json.append(data)  
+        for p in players:
+            lat, lon = utils.parseGeoPt(p.location)
+            data = {'name': p.name,'lat': lat,'lon': lon}
+            players_json.append(data)
+              
+        logging.info(_trace+'battles = '+simplejson.dumps(battles_json))
+        logging.info(_trace+'monsters = '+simplejson.dumps(monsters_json))
+        logging.info(_trace+'players = '+simplejson.dumps(players_json))
+        template_values = {
+            'players': simplejson.dumps(players_json),
+            'monsters': simplejson.dumps(monsters_json),
+            'battles': simplejson.dumps(battles_json)
+        }        
+        generate(self, 'test/test_home.html', template_values)
                      
 ######################## METHODS #############################################
 ##############################################################################
@@ -234,6 +269,7 @@ application = webapp.WSGIApplication([('/admin/', Admin),
                                       ('/admin/weapon', WeaponAdmin),
                                       ('/admin/power', PowerAdmin),
                                       (r'/admin/character/(.*)', CharacterAdmin),
+                                      (r'/admin/test/home', Home),
                                       (r'/admin/test/(.*)', Test)],
                                        debug=True)
 
